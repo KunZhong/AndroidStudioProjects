f5dda1da61d9227a14844696b9dc34ab8e366c8d
zhongkun0313 <zhongkun0313@thundersoft.com>
2017-04-24 16:52:56 +0800

    简易版-新闻应用

---
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/News.java
@@ -0,0 +1,30 @@
+package com.example.fragmentbestpractice;
+
+/**
+ * Created by user on 17-4-24.
+ */
+
+public class News {
+
+    private String title;
+
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
+
