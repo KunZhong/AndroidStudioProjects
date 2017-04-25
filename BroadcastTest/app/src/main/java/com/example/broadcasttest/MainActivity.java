fc199c0a992d922993250720cb32923a6a55b8f8
zhongkun0313 <zhongkun0313@thundersoft.com>
2017-04-25 11:17:26 +0800

    动态注册广播，监听网络变化，静态注册广播实现开机启动

---
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+
+    private NetworkChangeReceiver networkChangeReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+
+        networkChangeReceiver = new NetworkChangeReceiver();
+
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            ConnectivityManager connectionManager = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
+// 新建一个类继承自广播接收器Broadcast-Receiver，并重写父类的onReceive()方法
+// 动态注册方式编写一个能够监听网络变化的程序
+
+// 创建IntentFilter实例，添加android.net.conn.CONNECTIVITY_CHANGE，
+// 当网络状态发生变化时，系统发出的正是一条值为上面的广播
+// 然后registerReceiver()进行注册
+// 动态注册的广播接收器一定要取消注册才行
+
+// 测试：
+//      注册完成时候收到一条广播，然后按下Home返回主界面，关闭启动网络
+//      打开 Setting -> Data usage -> Cellular data 开关
